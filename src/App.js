5f0434c13ad1742e423dd4bd000888a7b46e4b40
Sallein <saleinkyl@gmail.com>
2023-10-16 21:28:36 +0300

    реализован функционал добавления фото

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Teseract from './components/Tesseract/Teseract';
 
 function App() {
   return (
     <div className="App">
-    <h1>тык</h1>
+    <Teseract />
     </div>
   );
 }
