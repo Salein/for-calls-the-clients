3ea3c0dc9ca546b6f5ab78d5c4ea67bf6365bcff
Sallein <saleinkyl@gmail.com>
2023-10-16 22:14:12 +0300

    багло в юзэфекте

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import './App.css';
-import Teseract from './components/Tesseract/Teseract';
+import React, { useEffect, useState } from "react"
+import { createWorker } from "tesseract.js"
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [textResult, setTextResult] = useState("")
+
+  const worker = createWorker()
+  
+// ниже багло
+  useEffect(() => {
+    const convertImageToText = async () => {
+      await worker.load()
+      await worker.loadLanguage("ru")
+      await worker.initialize("ru")
+      const { data } = await worker.recognize(selectedImage)
+      console.log(data.text)
+    }
+  }, [selectedImage])
+
+  const handleChangeImage = (e) => {
+    setSelectedImage(e.target.files[0])
+  }
+
   return (
     <div className="App">
-    <Teseract />
+      <input
+        type="file"
+        id="upload"
+        accept="image/*"
+        onChange={handleChangeImage}
+      />
+
+      <button type="button" id="start">
+        Начать обработку
+      </button>
+
+      <div id="log">
+        {selectedImage && (
+          <div>
+            <img src={URL.createObjectURL(selectedImage)} alt="thumb" />
+          </div>
+        )}
+        {textResult && <div>{textResult}</div>}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
